daa61729b1abf5950ab802528ad2a0cb77fc1145
Huy Trinh <trinhphandinhhuy@gmail.com>
2016-05-23 15:16:30 +0300

    Change API from IMDB to TheMovieDB

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,16 +1,25 @@
 var Movie = require('../model/movieModel');
 var User = require('../model/userModel');
 var nmovie = require('node-movie');
+var MovieDB = require('moviedb')('2ffb4d76855be971ac9d322ae367d589');
 
 exports.list = function (req, res) {
     Movie.findOneRandom(function (error, movie) {
         nmovie(movie.title, function (error, data) {
-            //console.log(movieInfo);
-            res.render('index1', {
-                title: 'Movie - Random',
-                movie: movie,
-                Plot: data.Plot,
-                Poster: data.Poster,
+            MovieDB.movieInfo({ id: data.imdbID }, function (err, result) {;
+                var poster;
+                if (!result){
+                    poster = 'http://cinematic.ge/uploads/posts/2014-04/1398024527_no-poster.jpg';
+                } else {
+                  poster = 'https://image.tmdb.org/t/p/original' + result.poster_path;  
+                }
+                
+                res.render('index1', {
+                    title: 'Movie - Random',
+                    movie: movie,
+                    Plot: data.Plot,
+                    Poster: poster,
+                });
             });
         });
     });
