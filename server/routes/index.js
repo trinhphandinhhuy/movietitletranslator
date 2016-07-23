0b03543eda734ddc1f3b8b3ccdc28c3eb670b333
Huy Trinh <trinhphandinhhuy@gmail.com>
2016-07-23 10:00:41 +0300

    clean up a bit

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,18 +4,11 @@ var movieController = require('../controllers/movieController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.send({ title: 'Express' });
-  return movieController.next(req, res);
-  //return movieController.one(req, res);
+  return movieController.next(req, res);;
 });
 
 router.get('/:index', function(req, res, next) {
   return movieController.next(req, res);
-});
-
-
-router.post('/', function(req, res, next) {
-  return movieController.one(req, res);
 });
 
 
